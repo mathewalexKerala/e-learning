ab219f3878b59b41f7575158fc837e89d14bc3bf
=Mathew Alex <mathewalex557@gmail.com>
2023-12-04 15:14:35 +0530

    'second commit

--- server/Routes/AuthRoute.js
+++ server/Routes/AuthRoute.js
@@ -1,6 +1,8 @@
-const { Signup } = require("../Controllers/AuthController");
+const { Signup ,Login,userVerification} = require("../Controllers/AuthController");
 const router = require("express").Router();
 
 router.post("/signup", Signup);
+router.post("/login", Login);
+router.post('/',userVerification)
 
 module.exports = router;
--- server/util/SecretToken.js
+++ server/util/SecretToken.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
-
+const {TOKEN_KEY}=process.env
+console.log(TOKEN_KEY)
 module.exports.createSecretToken = (id) => {
-  return jwt.sign({ id }, process.env.TOKEN_KEY, {
+  return jwt.sign({ id }, TOKEN_KEY, {
     expiresIn: 3 * 24 * 60 * 60,
   });
 };
